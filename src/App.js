f12c79a604f7b007441e186990ee1a8676204812
everydayday <96685431+everydayday@users.noreply.github.com>
2024-06-04 18:04:07 +0900

    Tabs & Maps

--- src/App.js
+++ src/App.js
@@ -2,27 +2,20 @@ import "./App.css";
 import { FcHome } from "react-icons/fc";
 import { IoHome } from "react-icons/io5";
 import { BrowserRouter, Routes, Route,Link } from "react-router-dom";
+import {Button, Tabs} from 'antd';
 
-// import HelloCss from "./02/HelloCss";
-// import logo from "./logo.svg";
-// import icons from "./icons";
-// import MyDiv from "./03_1/MyDiv";
-// import MyList from "./04/MyList";
-// import MyListMain from "./04/MyListMain";
-// import BoxOfficeTb from "./05_1/BoxOfficeTb";
-// import BoxOfficeTbody from "./05_1/BoxOfficeTb";
-// import FoodCard from "./07/FoodCard";
+import KoreaMap from "./Map/KoreaMap";
+
+import { ComposableMap, Geographies, Geography } from "react-simple-maps"
+
+
 import FoodMain from "./07/FoodMain";
- import Lotto from "./06/Lotto";
- import MyClock from "./08/MyClock";
-// import TrafficMain from "./09/TrafficMain";
-// import RefVal from "./10/RefVal";
-// import RefInput from "./10/RefInput";
+import Lotto from "./06/Lotto";
+import MyClock from "./08/MyClock";
+
 import BoxOffice from "./10/BoxOffice";
-// import GalleryCard from "./11/GalleryCard";
+
 import GalleryMain from "./11/GalleryMain";
-// import EventMain from "./12/EventMain";
-// import RouteMain from "./13/RouteMain";
 import Frcst from "./14/Frcst";
 import UltraSrtFcst from "./14/UltraSrtFcst";
 import VilageFcst from "./14/VilageFcst";
@@ -30,10 +23,15 @@ import FrcsList from "./14/FrcsList";
 import Recoil1 from "./15/Recoil1";
 import RecoilMain from "./15/RecoilMain";
 
+
+
+
 function App() {
   return (
     <BrowserRouter>
       {/* // JSX 문법 사용 */}
+
+      
       <div
         className="flex flex-col w-full
                           max-w-screen-xl
@@ -43,7 +41,7 @@ function App() {
                           "
       >
         <header
-          className="flex justify-between
+          className="flex justify-between 
                               h-20 p-10
                               text-xl font-bold text-cyan-800
                               bg-green-400
@@ -59,18 +57,55 @@ function App() {
             <div className="mx-2 hover:text-blue-300"><Link to='/gallerymain'>관광지</Link></div>
             <div className="mx-2 hover:text-blue-300"><Link to = '/boxoffice'>박스오피스</Link></div>
             <div className="mx-2 hover:text-blue-300"><Link to ='/Lotto'>로또</Link></div> 
-            
-            
             <div><Link to = '/'><IoHome className="hover:fill-blue-400"/></Link></div>
             
           </div>
         </header>
 
-        <main className="grow flex flex-col justify-center items-center">
-         
-          
+        <div className="bg-purple-400">
+        <div>준비된 당신이 도착할 지역을</div>
+        <div>한 눈에 보세요</div>
+        </div>
+        <Button type="primary">PRESS ME</Button>
+        
+        
+
+
+        <div className="bg-red-300">직종코드입력창</div>
+        <div className="flex flex-row h-full">
+        <div className="flex justify-center items-stretch h-full w-1/5">
+        <div className="bg-blue-200 w-48 flex-grow">
+          세로로 긴 영역
+        </div>
+
+        {/* https://ant.design/components/tabs */}
+
+        <Tabs defaultActiveKey="1"
+        tabPosition={'left'}
+        style={{
+          height: 220,
+        }}
+        items={new Array(5).fill(null).map((_, i) => {
+          const id = String(i);
+          return {
+            label: `Tab-${id}`,
+            key: id,
+            disabled: i === 28,
+            //children: `Content of tab ${id}`,
+          };
+        })}              
+              
+        />
+        </div>
+
+        <div>
+          <select>
+            <option value="A">A</option>
+          </select>
+        </div>
+        
+        <main className="grow flex flex-col justify-end items-center w-4/5">
           <Routes>
-         
             <Route path="/" element={<MyClock />} />
             <Route path="/lotto" element={<Lotto />} />
             <Route path="/boxOffice" element={<BoxOffice />}/>
@@ -84,7 +119,13 @@ function App() {
             <Route path="/recoil1" element = {<RecoilMain/>}/>
           </Routes>
         </main>
+        </div>
 
+
+        <div>
+          <h1>한반도 지도</h1>
+          {/* <KoreaMap /> */}
+        </div>
         <footer className="flex justify-center items-center  inset-x-0 bottom-0 h-16  text-white bg-slate-800">
           @ 2024 Kim Dae Hee. All rights reserved
         </footer>
---
+++ src/Map/KoreaMap.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+
+// 한반도 GeoJSON 데이터
+const geoUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-110m.json';
+
+const KoreaMap = () => {
+  return (
+    <ComposableMap projection="geoMercator" width={800} height={600}>
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.map((geo) => (
+            <Geography
+              key={geo.rsmKey}
+              geography={geo}
+              // style={{
+              //   default: {
+              //     fill: "#D6D6DA",
+              //     outline: "none",
+              //   },
+                // hover: {
+                //   fill: "#F53",
+                //   outline: "none",
+                // },
+                // pressed: {
+                //   fill: "#E42",
+                //   outline: "none",
+                // },
+              // }}
+            />
+          ))
+        }
+      </Geographies>
+    </ComposableMap>
+  );
+};
+
+export default KoreaMap;
